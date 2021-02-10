821638d38363244742991aafd794d444af29575f
oliver.bruentje <oliver.bruentje@openknowledge.de>
2021-02-10 11:46:22 +0100

    Plot diagrams

--- src/test_data.py
+++ src/test_data.py
@@ -12,6 +12,9 @@ class TrainDataSet:
     def cadence_to_power(self) -> tuple:
         return (DataFrame(data={tcx.COLUMN_NAME_CADENCE: self.df[tcx.COLUMN_NAME_CADENCE]}), self._get_power())
 
+    def cadence_to_speed(self) -> tuple:
+        return (DataFrame(data={tcx.COLUMN_NAME_CADENCE: self.df[tcx.COLUMN_NAME_CADENCE]}), self.df[tcx.COLUMN_NAME_SPEED])
+
     def speed_to_power(self) -> tuple:
         return (DataFrame(data={tcx.COLUMN_NAME_SPEED: self.df[tcx.COLUMN_NAME_SPEED]}), self._get_power())
 
